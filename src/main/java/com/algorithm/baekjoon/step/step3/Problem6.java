fb9882de17f3827c670aa613322cb539e16f8b22
jeongwon <jeongwon201@naver.com>
2022-10-28 16:55:43 +0900

    baekjoon step3

---
+++ src/main/java/com/algorithm/baekjoon/step/step3/Problem6.java
@@ -0,0 +1,29 @@
+package com.algorithm.baekjoon.step.step3;
+// 6. A + B - 7
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem6 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for(int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #" + (i+1) + ": " + (a + b) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
