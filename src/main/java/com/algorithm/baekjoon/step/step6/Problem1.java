46e20ce47bbf9c01f735b872e603d90fb1116dbe
jeongwon <jeongwon201@naver.com>
2022-11-04 18:18:43 +0900

    baekjoon step 6

---
+++ src/main/java/com/algorithm/baekjoon/step/step6/Problem1.java
@@ -0,0 +1,23 @@
+package com.algorithm.baekjoon.step.step6;
+// 1. 아스키 코드
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        char c = s.charAt(0);
+        bw.write((int)c + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
+
+// 아스키 코드 변환은 int 형변환하면 된다.
---
+++ src/main/java/com/algorithm/baekjoon/step/step6/Problem2.java
@@ -0,0 +1,28 @@
+package com.algorithm.baekjoon.step.step6;
+// 2. 숫자의 합
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        int sum = 0;
+        for(int i=0; i<s.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+
+        bw.write(sum + " ");
+        bw.flush();
+        bw.close();
+    }
+}
+
+// 아스키 코드로 변환 후 -48 해서 숫자로 바꾸는 방법도 있다.
